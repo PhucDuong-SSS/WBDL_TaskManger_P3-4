3d6445128a49891e2d5fa2d96f444bb3c481a78e
PhucDuong-SSS <phucdnDev@gmail.com>
2020-11-28 15:14:55 +0700

    Task manager p3 maker controller resource

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CustomerContrller;
+use App\Http\Controllers\TasKController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::resource([
+    'customer'=>CustomerContrller::class,
+    'task'=>TasKController::class,
+]);
